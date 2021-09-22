721b311f7723e655b317b083eb814bc91a68ec99
togami2864 <tuabobo123@gmail.com>
2021-09-23 03:04:29 +0900

    add class-component in test fixtures

---
+++ packages/astro/test/fixtures/react-component/src/components/ClassComponent.jsx
@@ -0,0 +1,9 @@
+import  { Component } from 'react';
+
+export default class ClassComponent extends Component{
+    render() {
+        return (
+            <div id="class-component">Hello World!</div>
+        )
+    }
+}
