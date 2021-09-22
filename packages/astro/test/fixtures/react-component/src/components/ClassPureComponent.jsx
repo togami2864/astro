0031142b1e32dd0f7d88789866f1b9c0f30f4f1b
togami2864 <tuabobo123@gmail.com>
2021-09-23 03:05:09 +0900

    add class-pure-component in test fixtures

---
+++ packages/astro/test/fixtures/react-component/src/components/ClassPureComponent.jsx
@@ -0,0 +1,9 @@
+import { PureComponent } from 'react';
+
+export default class ClassPureComponent extends PureComponent{
+    render() {
+        return (
+            <div id="pure-component">Hello World!</div>
+        )
+    }
+}
